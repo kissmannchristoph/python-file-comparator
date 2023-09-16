95449ed7ff0576fb1b0ca51877f440072728eed6
BuildTools <unconfigured@null.spigotmc.org>
2023-09-17 00:08:21 +0200

    feat: 🎸 fix workflow, add start.py

---
+++ start.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+MAIN_PATH = os.path.dirname(os.path.realpath(__file__))
+
+argsString = ""
+
+for arg in sys.argv:
+    argsString += " " + arg
+
+os.system("python main.py '" + MAIN_PATH + "'" + argsString)
